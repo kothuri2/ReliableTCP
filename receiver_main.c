80c7cd254e4884b14ff1fde995675d5525087c3d
Radhir Kothuri <kothuri2@illinois.edu>
2017-04-23 17:51:11 -0500

    set up port

--- receiver_main.c
+++ receiver_main.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+int globalSocketUDP;
 
 void reliablyReceive(unsigned short int myUDPport, char* destinationFile) {
+	struct sockaddr_in theirAddr;
+	socklen_t theirAddrLen;
+	unsigned char recvBuf [10];
+	int bytesRecvd;
+	theirAddrLen = sizeof(theirAddr);
+	printf("%s\n", "Waiting for sender...");
+	if ((bytesRecvd = recvfrom(globalSocketUDP, recvBuf, 2048, 0, 
+				(struct sockaddr*)&theirAddr, &theirAddrLen)) == -1)
+	{
+		perror("connectivity listener: recvfrom failed");
+		exit(1);
+	}
+	printf("%s\n", recvBuf);
+
 }
 
 int main(int argc, char** argv)
@@ -11,6 +35,26 @@ int main(int argc, char** argv)
 	if(argc != 3)
 	{
 		fprintf(stderr, "usage: %s UDP_port filename_to_write\n\n", argv[0]);
+		exit(1);
+	}
+
+	//socket() and bind() our socket. We will do all sendto()ing and recvfrom()ing on this one.
+	if((globalSocketUDP=socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+	{
+		perror("socket");
+		exit(1);
+	}
+	char myAddr[100];
+	struct sockaddr_in bindAddr;
+	sprintf(myAddr, "127.0.0.1");
+	memset(&bindAddr, 0, sizeof(bindAddr));
+	bindAddr.sin_family = AF_INET;
+	bindAddr.sin_port = htons(5000);
+	inet_pton(AF_INET, myAddr, &bindAddr.sin_addr);
+	if(bind(globalSocketUDP, (struct sockaddr*)&bindAddr, sizeof(struct sockaddr_in)) < 0)
+	{
+		perror("bind");
+		close(globalSocketUDP);
 		exit(1);
 	}
 	
--- sender_main.c
+++ sender_main.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+int globalSocketUDP;
 
 void reliablyTransfer(char* hostname, unsigned short int hostUDPport, char* filename, unsigned long long int bytesToTransfer) {
+	struct sockaddr_in receiver;
+	char tempaddr[100];
+	sprintf(tempaddr, "127.0.0.1");
+	memset(&receiver, 0, sizeof(receiver));
+	receiver.sin_family = AF_INET;
+	receiver.sin_port = htons(5000);
+	inet_pton(AF_INET, tempaddr, &receiver.sin_addr);
+	sendto(globalSocketUDP, "hello", 6, 0, (struct sockaddr*)&receiver, sizeof(receiver));
 }
 
 int main(int argc, char** argv)
@@ -14,6 +32,27 @@ int main(int argc, char** argv)
 		fprintf(stderr, "usage: %s receiver_hostname receiver_port filename_to_xfer bytes_to_xfer\n\n", argv[0]);
 		exit(1);
 	}
+
+	//socket() and bind() our socket. We will do all sendto()ing and recvfrom()ing on this one.
+	if((globalSocketUDP=socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+	{
+		perror("socket");
+		exit(1);
+	}
+	char myAddr[100];
+	struct sockaddr_in bindAddr;
+	sprintf(myAddr, "127.0.0.1");
+	memset(&bindAddr, 0, sizeof(bindAddr));
+	bindAddr.sin_family = AF_INET;
+	bindAddr.sin_port = htons(9000);
+	inet_pton(AF_INET, myAddr, &bindAddr.sin_addr);
+	if(bind(globalSocketUDP, (struct sockaddr*)&bindAddr, sizeof(struct sockaddr_in)) < 0)
+	{
+		perror("bind");
+		close(globalSocketUDP);
+		exit(1);
+	}
+
 	udpPort = (unsigned short int)atoi(argv[2]);
 	numBytes = atoll(argv[4]);
 	
